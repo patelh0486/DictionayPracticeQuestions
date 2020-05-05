e6cfa5861a44ea40568c14c3b63e8c8ef484b061
patelh0486 <patelh0486@uhcl.edu>
2020-05-05 17:16:38 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace FinalExamReview
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            String uname = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            String password = Console.ReadLine();
+            if (LoginInfo.ContainsKey(uname))
+            { 
+                if(LoginInfo[uname]==password)
+                {
+                    Console.WriteLine($" {uname} is loggedin.");
+                }
+            else
+                {
+                    Console.WriteLine("the assword entered is incorrect");
+                }
+            }
+            else
+            {
+
+                Console.WriteLine($" {uname} user not found.");
+            }
+            foreach(var kvp in LoginInfo)
+            {
+                Console.WriteLine($"{kvp.Key,10} {kvp.Value}");
+            }
+
+            string[] keys = new string[LoginInfo.Count];
+            LoginInfo.Keys.CopyTo(keys, 0);
+            for(int i = 0; i< keys.Length; i++)
+            {
+                Console.WriteLine($"{keys[i],10}");
+            }
+        }
+
+        static Program()
+        {
+            LoginInfo["hinal"] = "patel";
+            LoginInfo["michael"] = "wu";
+            LoginInfo["gunjan"] = "pabreja";
+        }
+
+        private static readonly Dictionary<string, string> LoginInfo = new Dictionary<string, string>
+        { 
+               {"hinal", "patel"},
+               {"kevin", "patel"},
+               {"binjal", "patel"},
+
+            };
+    }
+}
